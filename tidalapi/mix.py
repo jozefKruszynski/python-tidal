e2a69b77d85679b21c2988a80e7fbf497c166da7
jkruszynski <jozef@krush.at>
2023-03-29 20:47:41 +0200

    Add images to parsed mix

--- tidalapi/mix.py
+++ tidalapi/mix.py
@@ -26,17 +26,18 @@ class MixType(Enum):
     """
     An enum to track all the different types of mixes
     """
-    video_daily = 'VIDEO_DAILY_MIX'
-    daily = 'DAILY_MIX'
-    discovery = 'DISCOVERY_MIX'
-    new_release = 'NEW_RELEASE_MIX'
-    track = 'TRACK_MIX'
-    artist = 'ARTIST_MIX'
-    songwriter = 'SONGWRITER_MIX'
-    producter = 'PRODUCER_MIX'
-    history_alltime = 'HISTORY_ALLTIME_MIX'
-    history_monthly = 'HISTORY_MONTHLY_MIX'
-    history_yearly = 'HISTORY_YEARLY_MIX'
+
+    video_daily = "VIDEO_DAILY_MIX"
+    daily = "DAILY_MIX"
+    discovery = "DISCOVERY_MIX"
+    new_release = "NEW_RELEASE_MIX"
+    track = "TRACK_MIX"
+    artist = "ARTIST_MIX"
+    songwriter = "SONGWRITER_MIX"
+    producter = "PRODUCER_MIX"
+    history_alltime = "HISTORY_ALLTIME_MIX"
+    history_monthly = "HISTORY_MONTHLY_MIX"
+    history_yearly = "HISTORY_YEARLY_MIX"
 
 
 class Mix(object):
@@ -45,10 +46,12 @@ class Mix(object):
 
     These get used for many things, like artist/track radio's, recommendations, and historical plays
     """
+
     id = ""
     title = ""
     sub_title = ""
     sharing_images = None
+    images = None
     mix_type = None
     content_behaviour = ""
     short_subtitle = ""
@@ -71,28 +74,28 @@ class Mix(object):
         if mix_id is None:
             mix_id = self.id
 
-        params = {'mixId': mix_id,
-                  'deviceType': 'BROWSER'}
+        params = {"mixId": mix_id, "deviceType": "BROWSER"}
         parse = self.session.parse_page
-        result = self.request.map_request('pages/mix', parse=parse, params=params)
+        result = self.request.map_request("pages/mix", parse=parse, params=params)
         self._retrieved = True
         self.__dict__.update(result.categories[0].__dict__)
         self._items = result.categories[1].items
         return self
 
     def parse(self, json_obj):
-        """ Parse a mix into a :class:`Mix`, replaces the calling object
+        """Parse a mix into a :class:`Mix`, replaces the calling object
 
         :param json_obj: The json of a mix to be parsed
         :return: A copy of the parsed mix
         """
-        self.id = json_obj['id']
-        self.title = json_obj['title']
-        self.sub_title = json_obj['subTitle']
-        self.sharing_images = json_obj['sharingImages']
-        self.mix_type = MixType(json_obj['mixType'])
-        self.content_behaviour = json_obj['contentBehavior']
-        self.short_subtitle = json_obj['shortSubtitle']
+        self.id = json_obj["id"]
+        self.title = json_obj["title"]
+        self.sub_title = json_obj["subTitle"]
+        self.sharing_images = json_obj["sharingImages"]
+        self.images = json_obj["images"]
+        self.mix_type = MixType(json_obj["mixType"])
+        self.content_behaviour = json_obj["contentBehavior"]
+        self.short_subtitle = json_obj["shortSubtitle"]
 
         return copy.copy(self)
 
